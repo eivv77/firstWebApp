81a4ba8d4bc8a0c0d513f2185ed19513f7ac658d
eivv77 <dlya.kf.2002@mail.ru>
2020-08-26 14:11:43 +0400

    init

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    //delete
+    public function delete(){
+        echo "delete product";
+    }
+
+
+    //store
+    public function create(){
+        echo "store product";
+    }
+}
